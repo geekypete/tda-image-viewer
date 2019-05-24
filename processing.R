dfdef366d63847193077a9525ef1c9b92fa6cc53
Pete Lawson <geekypete@gmail.com>
2019-05-24 12:09:10 -0500

    Added processing code

---
+++ processing.R
@@ -0,0 +1,29 @@
+use_package('doParallel')
+use_package('foreach')
+use_package('stringr')
+use_package('TDA')
+use_package('reshape2')
+use_package('ggplot2')
+use_package('png')
+use_package('shiny')
+use_package('shinydashboard')
+use_package('plotly')
+
+image_path <- "./images/miccai-2015/"
+files <- list.files(image_path, pattern="\\.png$" )
+files_with_path <- paste(image_path, files, sep = "")
+
+num_cores <- detectCores()
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+persistence_diagrams <- foreach(i=files, .packages=c('TDA', 'png')) %dopar%{
+    img_path <- paste(image_path, i, sep = "")
+    img_mat <- readPNG(img_path) 
+    # Luminance preserving grayscale conversion
+    img_mat <- 0.2126*img_mat[,,1] + 0.7152*img_mat[,,2] + 0.0722*img_mat[,,3]
+    # Generate persistence diagram
+    tmp_diag <- gridDiag(FUNvalues=img_mat , sublevel=TRUE, 
+                          library="Dionysus", printProgress=FALSE, location=TRUE)
+    list(i, tmp_diag)
+}
