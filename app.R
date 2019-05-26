f155b5ee75c8560d1635bfdaa2e123406d56b5fb
Pete Lawson <geekypete@gmail.com>
2019-05-26 17:28:02 -0500

    Summ

--- app.R
+++ app.R
@@ -1,18 +1,25 @@
 rm(list=ls())
 source('./utility-functions.R')
-source('./processing.R')
-
+#source('./processing.R')
+persistence_diagrams <- readRDS('./perstdiag.rds')
 ui <- dashboardPage(
   dashboardHeader(title="TDA Image Viewer"),
   dashboardSidebar(
-      sidebarMenu(
-          menuItem("Dashboard", tabName = "dashboard"),
-          menuItem("Distance Measures", tabName = "distance")
-      )),
-    dashboardBody()
-  )
+  selectInput("file", "ROI", files, width=200)),
+  dashboardBody(
+      fluidRow(
+          box(title = "Persistence",  status = "primary", plotOutput("cycleA"))
+      )
+  ))
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+
+  output$cycleA<- renderPlot({
+      img_index <- match(input$file, files)
+      print(img_index)
+      perstImage(paste(image_path, input$file, sep=''), persistence_diagrams[[img_index]][[2]], .1, 1, betti=1, image='thresh')
+  },height=500, width=700)
+}
 
 options(shiny.host = '0.0.0.0')
 options(shiny.port = 8080)
