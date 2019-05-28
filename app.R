4bb98b1060b80eaf8471ece35488096940599b2f
Pete Lawson <geekypete@gmail.com>
2019-05-28 13:14:32 -0500

    Revised plot dimensions

--- app.R
+++ app.R
@@ -4,17 +4,24 @@ source('./utility-functions.R')
 persistence_diagrams <- readRDS('./perstdiag.rds')
 ui <- dashboardPage(
   dashboardHeader(title="TDA Image Viewer"),
-  dashboardSidebar(
-  selectInput("file", "ROI", files, width=200),
-  sliderInput("min_perst","Minimum Persistence Bound", min=0, max=1, value=0, width=200),
-  sliderInput("max_perst","Maximum Persistence Bound", min=0, max=1, value=1, width=200),
-  selectInput("img_type", "Image Type", c("Color", "Grayscale", "Threshold"), width=200)),
+  dashboardSidebar(disable=TRUE),
   dashboardBody(
+
       fluidRow(
-          box(status = "primary", plotOutput("cycleA"), width=5, height=520)
-      ),
+          box(status = "primary", plotOutput("cycleA"), width=12, height=520)
+          ),
+
       fluidRow(
-          box(title = "Persistence", status="primary", plotOutput("perst"), width=3, height=600)
+          box(title = "Persistence", status="primary", plotOutput("perst"), width=10, height=600),
+          box(title = "Parameters", status="warning",
+              selectInput("file", "ROI", files, width=200),
+              sliderInput("min_perst","Minimum Persistence Bound", min=0, max=1, value=0, width=200),
+              sliderInput("max_perst","Maximum Persistence Bound", min=0, max=1, value=1, width=200),
+              radioButtons("img_type", "Image Type", c("Color", "Grayscale", "Threshold"), selected="Color"),
+              checkboxInput("show_cycles", "Show Representative Cycles", TRUE),
+              checkboxInput("show_bg", "Show Background Image", TRUE),
+              width = 2
+              )
       )
   ))
 
@@ -22,8 +29,8 @@ server <- function(input, output, session) {
 
   output$cycleA<- renderPlot({
       img_index <- match(input$file, files)
-      perstImage(paste(image_path, input$file, sep=''), persistence_diagrams[[img_index]][[2]], input$min_perst, input$max_perst, betti=1, image=input$img_type)
-  },height=500, width=700)
+      perstImage(paste(image_path, input$file, sep=''), persistence_diagrams[[img_index]][[2]], input$min_perst, input$max_perst, betti=1, image=input$img_type, show_cycles = input$show_cycles, show_bg = input$show_bg)
+  },height=600, width=750)
 
 output$perst<- renderPlot({
     img_index <- match(input$file, files)
