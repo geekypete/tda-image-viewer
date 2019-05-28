cce4fc418ae30feab4ccd6b848af70c0ada078ff
Pete Lawson <geekypete@gmail.com>
2019-05-28 16:26:04 -0500

    Inverted threshold representation

--- utility-functions.R
+++ utility-functions.R
@@ -13,7 +13,7 @@ use_package <- function(p) {
 #' @param file filename and path to .png image to overlay cycles on
 #' @param perst list in the form list(persistenceDiagram, nucleiXYcoordinates)
 #' @export
-perstImage <- function(file, diagram, min, max, betti, image="Color", generators=TRUE, show_cycles=TRUE, show_bg=TRUE){
+perstImage <- function(file, diagram, min, max, birth, image="Color", generators=TRUE, show_cc = FALSE, show_cycles=TRUE, show_bg=TRUE){
     if(image=="Color")
         {
           img<-readPNG(file)
@@ -35,33 +35,38 @@ perstImage <- function(file, diagram, min, max, betti, image="Color", generators
     if(image=="Threshold")
     {
         img <- grayscale(load.image(file))
-        img <- as.cimg(threshold(img, (max+min)/2))
+        img <- as.cimg(threshold(img, birth))
         plot(NULL, type='n', ann=FALSE, main="Persistence", xlab="x", ylab="y",xlim =range(0:dim(img)[1]),ylim = rev(range(dim(img)[2]:0)),yaxs="i",xaxs="i")
         if(show_bg){
-            rasterImage(img, 0, dim(img)[2], dim(img)[1], 0)
+            rasterImage(1-img, 0, dim(img)[2], dim(img)[1], 0)
         }
     }
     if(show_cycles){
-      cycl <- getCycles(diagram, min, max, betti=betti)
+      cycl <- getCycles(diagram, min, max, betti=1, birth)
       points(cycl[[2]],cycl[[1]], col='chartreuse3', pch=19, cex=.25)
+    }
+
+    if(show_cc){
+        cycl <- getCycles(diagram, min, max, betti=0, birth)
+        points(cycl[[2]],cycl[[1]], col='red', pch=19, cex=.25)
     }
 }
 
 
 #' Retrieves the cycles for a given persistence diagram
 #' @param diag Persistence diagram to retrieve cycles from
-#' @param betti Boolean if True return only betti 1, else return all
+#' @param betti Return cycles corresponding to provided Betti number 
 #' @return return a 2*N list of x and y coordinates of cycles for given diagram
-getCycles<-function(diag, min, max, betti)
+getCycles<-function(diag, min, max, betti, birth)
 {
 
   if(betti==0){
-        X<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==0 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max)], '[',,,1))
-        Y<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==0 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max)], '[',,,2))
+        X<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==0 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max & diag$diagram[,2]<birth)], '[',,,1))
+        Y<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==0 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max & diag$diagram[,2]<birth)], '[',,,2))
   }
     if(betti==1){
-        X<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==1 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max)], '[',,,1))
-        Y<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==1 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max)], '[',,,2))
+        X<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==1 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max & diag$diagram[,2]<birth)], '[',,,1))
+        Y<-unlist(lapply(diag$cycleLocation[which(diag$diagram[,1]==1 & diag$diagram[,3]-diag$diagram[,2]>min & diag$diagram[,3]-diag$diagram[,2]<max & diag$diagram[,2]<birth)], '[',,,2))
   }
     return(list(X,Y))
 }
