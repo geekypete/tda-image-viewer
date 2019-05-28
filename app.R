9d111bdbc211fc44986ec25df8f09b6d63ba6156
Pete Lawson <geekypete@gmail.com>
2019-05-28 14:59:05 -0500

    Added to app

--- app.R
+++ app.R
@@ -19,6 +19,7 @@ ui <- dashboardPage(
               sliderInput("max_perst","Maximum Persistence Bound", min=0, max=1, value=1, width=200),
               radioButtons("img_type", "Image Type", c("Color", "Grayscale", "Threshold"), selected="Color"),
               checkboxInput("show_cycles", "Show Representative Cycles", TRUE),
+              checkboxInput("show_cc", "Show Representative Connected Components", FALSE),
               checkboxInput("show_bg", "Show Background Image", TRUE),
               width = 2
               )
@@ -27,18 +28,20 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
 
+  img <-readPNG(paste(image_path, files[1], sep=''))
+  img_height = nrow(img[,1,])*1.2
+  img_width = nrow(img[2,,])*1.2
   output$cycleA<- renderPlot({
       img_index <- match(input$file, files)
-      perstImage(paste(image_path, input$file, sep=''), persistence_diagrams[[img_index]][[2]], input$min_perst, input$max_perst, betti=1, image=input$img_type, show_cycles = input$show_cycles, show_bg = input$show_bg)
-  },height=600, width=750)
+      perstImage(paste(image_path, input$file, sep=''), persistence_diagrams[[img_index]][[2]], input$min_perst, input$max_perst, image=input$img_type, show_cycles = input$show_cycles, show_cc = input$show_cc, show_bg = input$show_bg)
+  },height=img_height, width=img_width)
 
 output$perst<- renderPlot({
     img_index <- match(input$file, files)
-    pd_at_index <- persistence_diagrams[[img_index]][[2]]$diag
-    pd_betti <- pd_at_index[which(pd_at_index[,1] == 1),]
-    pd_df <- data.frame(Birth=pd_betti[,2], Persistence = pd_betti[,3]-pd_betti[,2])
+    pd_betti <- persistence_diagrams[[img_index]][[2]]$diag
+    pd_df <- data.frame(Birth=pd_betti[,2], Persistence = pd_betti[,3]-pd_betti[,2], Betti = factor(pd_betti[,1]))
     pd_df <- pd_df[which(pd_df$Persistence > input$min_perst),]
-    ggplot(pd_df, aes(Birth, Persistence)) + geom_point() +
+    ggplot(pd_df, aes(Birth, Persistence)) + geom_point(aes(colour = Betti)) +
         scale_x_continuous(limits = c(0,1),breaks=seq(0,1,.2)) +
         scale_y_continuous(limits = c(0,1),breaks=seq(0,1,.2)) +
         theme_bw() +
